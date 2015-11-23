fca4a913d58038f442fd4d83a250975da50cc754
akihito.nakano <sora.akatsuki@gmail.com>
2015-11-23 10:10:24 +0900

    Add test

--- tests/Snidel/LogTest.php
+++ tests/Snidel/LogTest.php
@@ -12,4 +12,30 @@ class Snidel_LogTest extends PHPUnit_Framework_TestCase
         $this->assertObjectHasAttribute('destination', $log);
         fclose($fp);
     }
+
+    /**
+     * @test
+     */
+    public function info()
+    {
+        $log = new Snidel_Log(getmypid());
+        $fp = fopen('php://temp', 'w');
+        $log->setdestination($fp);
+        $log->info('test');
+        rewind($fp);
+        $this->assertStringMatchesFormat('%s[info]%s', fgets($fp));
+    }
+
+    /**
+     * @test
+     */
+    public function error()
+    {
+        $log = new Snidel_Log(getmypid());
+        $fp = fopen('php://temp', 'w');
+        $log->setdestination($fp);
+        $log->error('test');
+        rewind($fp);
+        $this->assertStringMatchesFormat('%s[error]%s', fgets($fp));
+    }
 }
