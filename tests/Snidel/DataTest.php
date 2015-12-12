6415ef93273a5357615893c5dbd6c1aef57e4151
akihito.nakano <sora.akatsuki@gmail.com>
2015-12-12 17:58:37 +0900

    Add test

--- tests/Snidel/DataTest.php
+++ tests/Snidel/DataTest.php
@@ -22,4 +22,88 @@ class Snidel_DataTest extends PHPUnit_Framework_TestCase
         $data->write('foo');
         $this->assertSame($data->readAndDelete(), 'foo');
     }
+
+    /**
+     * @test
+     * @expectedException RuntimeException
+     * @requires PHP 5.3
+     */
+    public function writeThrowsRuntimeExceptionWhenFailedToOpenShm()
+    {
+        $shm = $this->getMockBuilder('Snidel_SharedMemory')
+            ->setConstructorArgs(array(getmypid()))
+            ->setMethods(array('open'))
+            ->getMock();
+
+        $shm->method('open')
+            ->will($this->throwException(new RuntimeException));
+
+        $data = new Snidel_Data(getmypid());
+        $ref = new ReflectionProperty($data, 'shm');
+        $ref->setAccessible(true);
+        $ref->setValue($data, $shm);
+        try {
+            $data->write('foo');
+        } catch (RuntimeException $e) {
+            $data->delete();
+            throw $e;
+        }
+    }
+
+    /**
+     * @test
+     * @expectedException RuntimeException
+     * @requires PHP 5.3
+     */
+    public function writeThrowsRuntimeExceptionWhenFailedToWriteData()
+    {
+        $shm = $this->getMockBuilder('Snidel_SharedMemory')
+            ->setConstructorArgs(array(getmypid()))
+            ->setMethods(array('write'))
+            ->getMock();
+
+        $shm->expects($this->once())
+            ->method('write')
+            ->will($this->throwException(new RuntimeException));
+
+        $data = new Snidel_Data(getmypid());
+        $ref = new ReflectionProperty($data, 'shm');
+        $ref->setAccessible(true);
+        $ref->setValue($data, $shm);
+        try {
+            $data->write('foo');
+        } catch (RuntimeException $e) {
+            $data->delete();
+            throw $e;
+        }
+    }
+
+    /**
+     * @test
+     * @expectedException RuntimeException
+     * @requires PHP 5.3
+     */
+    public function readThrowsRuntimeException()
+    {
+        $shm = $this->getMockBuilder('Snidel_SharedMemory')
+            ->setConstructorArgs(array(getmypid()))
+            ->setMethods(array('read'))
+            ->getMock();
+
+        $shm->expects($this->once())
+            ->method('read')
+            ->will($this->throwException(new RuntimeException));
+
+        $data = new Snidel_Data(getmypid());
+        $data->write('foo');
+        $ref = new ReflectionProperty($data, 'shm');
+        $ref->setAccessible(true);
+        $ref->setValue($data, $shm);
+        try {
+            $data->read();
+        } catch (RuntimeException $e) {
+            $data->delete();
+            throw $e;
+        }
+    }
 }
