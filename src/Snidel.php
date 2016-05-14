922e8abf8e7482edfc7f95fbebf59eff41a7854e
akihito.nakano <sora.akatsuki@gmail.com>
2016-05-14 18:33:19 +0900

    Bump up version

--- src/Snidel.php
+++ src/Snidel.php
@@ -16,7 +16,7 @@ use Ackintosh\Snidel\Exception\SharedMemoryControlException;
 class Snidel
 {
     /** @var string */
-    const VERSION = '0.6.2';
+    const VERSION = '0.6.3';
 
     /** @var \Ackintosh\Snidel\ForkContainer */
     private $forkContainer;
