20682af75747cc3c2d05278679f6af1d5c362f51
akihito.nakano <sora.akatsuki@gmail.com>
2016-05-24 20:45:58 +0900

    Add test

---
+++ tests/Snidel/Task/TaskTest.php
@@ -0,0 +1,17 @@
+<?php
+use Ackintosh\Snidel\Task\Task;
+
+class TaskTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function execute()
+    {
+        $task = new Task(function ($arg) {
+            return 'foo' . $arg;
+        }, 'bar', null);
+
+        $this->assertInstanceOf('\Ackintosh\Snidel\Result\Result', $task->execute());
+    }
+}
