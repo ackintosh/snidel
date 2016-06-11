2abe5b6b356459125f4f2ea5743d8f929c7d9d98
akihito.nakano <sora.akatsuki@gmail.com>
2016-06-11 20:58:13 +0900

    Add test

--- tests/Snidel/WorkerTest.php
+++ tests/Snidel/WorkerTest.php
@@ -3,6 +3,8 @@ use Ackintosh\Snidel\Worker;
 use Ackintosh\Snidel\Result\Queue;
 use Ackintosh\Snidel\Fork\Fork;
 use Ackintosh\Snidel\Task\Task;
+use Ackintosh\Snidel\Fork\Container;
+use Ackintosh\Snidel\Log;
 
 class WorkerTest extends \PHPUnit_Framework_TestCase
 {
@@ -125,5 +127,44 @@ class WorkerTest extends \PHPUnit_Framework_TestCase
 
         $this->worker->setResultQueue($queue);
         $this->worker->error();
+    }
+
+    /**
+     * @test
+     */
+    public function terminate()
+    {
+        $container = $this->makeForkContainer();
+
+        $refProp = new \ReflectionProperty('\Ackintosh\Snidel\Fork\Container', 'masterPid');
+        $refProp->setAccessible(true);
+        $refProp->setValue($container, getmypid());
+
+        $refMethod = new \ReflectionMethod('\Ackintosh\Snidel\Fork\Container', 'forkWorker');
+        $refMethod->setAccessible(true);
+        $task = new Task(
+                function ($arg) {
+                    sleep(10);
+                    return 'foo' . $arg;
+                },
+                'bar',
+                null
+            );
+        $worker = $refMethod->invokeArgs($container, array($task));
+
+        $worker->terminate(SIGTERM);
+
+        // pcntl_wait with WUNTRACED returns `-1` if process has already terminated.
+        $status = null;
+        $this->assertSame(-1, pcntl_waitpid($worker->getPid(), $status, WUNTRACED));
+    }
+
+    public function makeForkContainer()
+    {
+        return new Container(
+            getmypid(),
+            new Log(getmypid()),
+            3
+        );
     }
 }
