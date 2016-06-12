42bdbe0da2c4a84447b32422f2d73765b4a303f9
akihito.nakano <sora.akatsuki@gmail.com>
2016-06-12 18:22:06 +0900

    Add test

--- tests/Snidel/ActiveWorkerSetTest.php
+++ tests/Snidel/ActiveWorkerSetTest.php
@@ -4,6 +4,8 @@ use Ackintosh\Snidel\Result\Queue;
 use Ackintosh\Snidel\Fork\Fork;
 use Ackintosh\Snidel\Task\Task;
 use Ackintosh\Snidel\ActiveWorkerSet;
+use Ackintosh\Snidel\Fork\Container;
+use Ackintosh\Snidel\Log;
 
 class ActiveWorkerSetTest extends \PHPUnit_Framework_TestCase
 {
@@ -65,6 +67,40 @@ class ActiveWorkerSetTest extends \PHPUnit_Framework_TestCase
         $this->assertSame(2, $this->activeWorkerSet->count());
     }
 
+    /**
+     * @test
+     */
+    public function terminate()
+    {
+        $container = $this->makeForkContainer();
+
+        $refProp = new \ReflectionProperty('\Ackintosh\Snidel\Fork\Container', 'masterPid');
+        $refProp->setAccessible(true);
+        $refProp->setValue($container, getmypid());
+
+        $refMethod = new \ReflectionMethod('\Ackintosh\Snidel\Fork\Container', 'forkWorker');
+        $refMethod->setAccessible(true);
+        $task = new Task(
+                function ($arg) {
+                    sleep(10);
+                    return 'foo' . $arg;
+                },
+                'bar',
+                null
+            );
+        $worker1 = $refMethod->invokeArgs($container, array($task));
+        $worker2 = $refMethod->invokeArgs($container, array($task));
+        $this->activeWorkerSet->add($worker1);
+        $this->activeWorkerSet->add($worker2);
+
+        $this->activeWorkerSet->terminate(SIGTERM);
+
+        // pcntl_wait with WUNTRACED returns `-1` if process has already terminated.
+        $status = null;
+        $this->assertSame(-1, pcntl_waitpid($worker1->getPid(), $status, WUNTRACED));
+        $this->assertSame(-1, pcntl_waitpid($worker2->getPid(), $status, WUNTRACED));
+    }
+
     private function makeWorker($pid = null)
     {
         $pid = $pid ?: getmypid();
@@ -78,6 +114,15 @@ class ActiveWorkerSetTest extends \PHPUnit_Framework_TestCase
                 'bar',
                 null
             )
+        );
+    }
+
+    private function makeForkContainer()
+    {
+        return new Container(
+            getmypid(),
+            new Log(getmypid()),
+            3
         );
     }
 }
