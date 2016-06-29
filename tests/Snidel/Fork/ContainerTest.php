f039b04a70bacbae45efa23f32fbf404733ca15d
akihito.nakano <sora.akatsuki@gmail.com>
2016-06-29 21:38:35 +0900

    Add test

--- tests/Snidel/Fork/ContainerTest.php
+++ tests/Snidel/Fork/ContainerTest.php
@@ -1,13 +1,14 @@
 <?php
-
+use Ackintosh\Snidel\TestCase;
 use Ackintosh\Snidel\Fork\Container;
 use Ackintosh\Snidel\Log;
+use Ackintosh\Snidel\Pcntl;
 use Ackintosh\Snidel\Task\Task;
 
 /**
  * @runTestsInSeparateProcesses
  */
-class ContainerTest extends \PHPUnit_Framework_TestCase
+class ContainerTest extends TestCase
 {
     /**
      * @test
@@ -26,5 +27,31 @@ class ContainerTest extends \PHPUnit_Framework_TestCase
 
         require_once(__DIR__ . '/../../msg_send.php');
         $container->enqueue($task);
+    }
+
+    /**
+     * @test
+     * @expectedException \RuntimeException
+     */
+    public function forkThrowsExceptionWhenFailed()
+    {
+
+        $pcntl = $this->getMockBuilder(Pcntl::class)
+            ->setMethods(array('fork'))
+            ->getMock();
+
+        $pcntl->expects($this->once())
+            ->method('fork')
+            ->will($this->returnValue(-1));
+
+        $container = $this->makeForkContainer();
+        $prop = new \ReflectionProperty($container, 'pcntl');
+        $prop->setAccessible(true);
+        $prop->setValue($container, $pcntl);
+
+        $method = new \ReflectionMethod(Container::class, 'fork');
+        $method->setAccessible(true);
+
+        $method->invoke($container);
     }
 }
